cff841c30623287a67cab6410d022b469543fb4f
dasha <hristoforovadasha23@yandex.ru>
2024-01-03 18:22:00 +0300

    RPP

---
+++ RPP/rpp.h
@@ -0,0 +1,14 @@
+//
+// Created by dasha on 1/2/24.
+//
+
+#ifndef COMPLEXITY_PROJECT_RPP_H
+#define COMPLEXITY_PROJECT_RPP_H
+
+
+class rpp {
+
+};
+
+
+#endif //COMPLEXITY_PROJECT_RPP_H
---
+++ RPP/test.cpp
@@ -0,0 +1,3 @@
+//
+// Created by dasha on 1/3/24.
+//
